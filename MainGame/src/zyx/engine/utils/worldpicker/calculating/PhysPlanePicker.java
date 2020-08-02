f46e7f7358e607abd1c9371c79ee66456ab8c4a8
Rene <zyxakarene@hotmail.com>
2020-08-02 14:54:52 +0200

    Another attempt at convex collisions

--- MainGame/src/zyx/engine/utils/worldpicker/calculating/PhysPlanePicker.java
+++ MainGame/src/zyx/engine/utils/worldpicker/calculating/PhysPlanePicker.java
@@ -3,12 +3,15 @@ package zyx.engine.utils.worldpicker.calculating;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 import zyx.opengl.models.implementations.physics.PhysBox;
+import zyx.opengl.models.implementations.physics.PhysObject;
 import zyx.opengl.models.implementations.physics.PhysTriangle;
 import zyx.utils.cheats.Print;
 import zyx.utils.interfaces.IPhysbox;
 
 public class PhysPlanePicker extends AbstractPicker
 {
+	private final Matrix4f INVERT_TRANSPOSE = new Matrix4f();
+	private final Matrix4f BONE_TRANSFORM = new Matrix4f();
 
 	@Override
 	public boolean collided(Vector3f pos, Vector3f dir, IPhysbox physContainer, Vector3f intersectPoint)
@@ -19,58 +22,68 @@ public class PhysPlanePicker extends AbstractPicker
 		{
 			return false;
 		}
-		boolean collided = false;
 
-		PhysTriangle[] triangles = phys.getTriangles();
-		if (triangles.length == 0)
+		boolean hitOBB = RayOBB.hit(phys.getBoundingBox(), mat, pos, dir);
+		if (!hitOBB)
 		{
 			return false;
 		}
-
-		Vector3f endPos = new Vector3f(pos);
-		endPos.x += (dir.x * 10000);
-		endPos.y += (dir.y * 10000);
-		endPos.z += (dir.z * 10000);
-
-		int negativeCount = 0;
-		for (PhysTriangle triangle : triangles)
+		
+		PhysObject[] objects = phys.getObjects();
+		
+		Vector3f end = new Vector3f();
+		end.x = pos.x + (10000 * dir.x);
+		end.y = pos.y + (10000 * dir.y);
+		end.z = pos.z + (10000 * dir.z);
+		
+		for (PhysObject object : objects)
 		{
-			collided = testTriangle(pos, endPos, triangle, mat);
+			PhysTriangle[] triangles = object.getTriangles();
+			short boneId = object.getBoneId();
+			Matrix4f boneMatrix = physContainer.getBoneMatrix(boneId);
+			
+			Matrix4f.mul(mat, boneMatrix, BONE_TRANSFORM);
 
-			if (!collided)
+			Matrix4f.load(BONE_TRANSFORM, INVERT_TRANSPOSE);
+			Matrix4f.invert(INVERT_TRANSPOSE, INVERT_TRANSPOSE);
+			Matrix4f.transpose(INVERT_TRANSPOSE, INVERT_TRANSPOSE);
+			
+			tE = 0f;
+			tL = 1f;
+			for (PhysTriangle triangle : triangles)
 			{
-				negativeCount++;
-				return false;
+				boolean collided = testTriangle(pos, end, dir, triangle, BONE_TRANSFORM, intersectPoint);
+				if (collided)
+				{
+					return true;
+				}
 			}
 		}
-		Print.out(negativeCount, triangles.length);
 
-		intersectPoint.set(pos);
-		return collided;
+		return false;
 	}
+	
+	private float tE;
+	private float tL;
 
-	private boolean testTriangle(Vector3f startPos, Vector3f endPos, PhysTriangle triangle, Matrix4f matrix)
+	private boolean testTriangle(Vector3f p0, Vector3f p1, Vector3f dS, PhysTriangle face, Matrix4f mat, Vector3f intersectPoint)
 	{
-		VERTEX_1.set(triangle.v1);
-		VERTEX_2.set(triangle.v2);
-		VERTEX_3.set(triangle.v3);
-		NORMAL.set(triangle.normal);
+		NORMAL.set(face.normal);
 
-		transformVertex(VERTEX_1, matrix);
-		transformVertex(VERTEX_2, matrix);
-		transformVertex(VERTEX_3, matrix);
-		transformVertex(NORMAL, matrix);
+		transformVertex(NORMAL, INVERT_TRANSPOSE);
 
-		float dotStart = Vector3f.dot(startPos, NORMAL);
-		float dotEnd = Vector3f.dot(endPos, NORMAL);
-
-		if ((dotStart > 0 && dotEnd < 0) || (dotStart < 0 && dotEnd > 0))
+		float diff = Vector3f.dot(NORMAL, dS);
+		if (diff > 0)
 		{
-			return true;
-		}
-		else
-		{
+			//Backfacing triangle
 			return false;
 		}
+		VERTEX_1.set(face.v1);
+		transformVertex(VERTEX_1, mat);
+		
+		Vector3f.dot(dS, dS)
+		//http://geomalgorithms.com/a13-_intersect-4.html#Intersect%20Segment%20and%20Polyhedron
+		
+		return false;
 	}
 }
